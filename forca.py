abde982dd9021fda9ddf00e423e835ef19417f90
Renato Gonçalves Pires <renato.goncalves.pires@gmail.com>
2018-03-20 23:59:46 -0300

    Lógica do jogo corrigida e agora as palavras secretas (aleatórias) vem de um arquivo .txt

--- forca.py
+++ forca.py
@@ -1,55 +1,48 @@
+import random
+
 def jogar():
     print("******************************")
     print("Bem-vindo(a) ao jogo da forca!")
     print("******************************")
 
-    #Define a palavra secreta
-    palavra_secreta = "banana"
-    
-    #Define o tamanho da palavra que será preenchida pelos underscores
+    arquivo = open("frutas.txt", "r")
+    frutas = []
+
+    for linha in arquivo:
+        frutas.append(linha.strip())
+
+    arquivo.close()
+    lista_frutas = random.randrange(0, len(frutas))
+    palavra_secreta = frutas[lista_frutas]
     tamanho_palavra = len(palavra_secreta)
-    
-    #Inicializa o número de tentativas
     tentativa = 1
-    
-    #Inicializa o vetor
     letra_posicao = []
-
-    #Preenche o vetor com underscores baseados no tamanho da palavra secreta
-    for i in range(tamanho_palavra):
-        letra_posicao.append("_")
-    
-    print("Essa é a palavra a ser adivinhada, você tem 5 chances.: {}".format(letra_posicao))
-
-    #Inicializa as variáveis de derrota e acerto
     enforcou = False
     acertou = False
 
-    #Enquanto não enforca (5 chances) e não acerta, continua o jogo
-    while (not enforcou and not acertou):
-        
-        #Se existem underscores na lista, é porquê o jogo não acabou
-        if "_" in letra_posicao:
+    for i in range(tamanho_palavra):
+        letra_posicao.append("_")
+    
+    print("Essa é a palavra a ser adivinhada, você tem {} chances.: {}".format(tamanho_palavra, letra_posicao))
+
+    while (not enforcou and not acertou):    
+        if "_" in letra_posicao and tentativa <= tamanho_palavra:
             print("Tentativa número: {}".format(tentativa))
-            
-            #Tratamento para espaços e letras maíusculas
-            chute = input("Digite um letra: ").strip().lower()
-            pos = 0
-        
-            #Se a letra do chute estiver na palavra secreta, coloca a mesma nas posições correspondentes
+            chute = input("Digite uma letra: ").strip().lower()
+            pos = 0 
+
             for letra in palavra_secreta:
                 if(letra == chute):
                     letra_posicao[pos] = chute
-                pos = pos + 1
-
-            #Verifica se há tentativas restantes para poder continuar o jogo
-            if tentativa == 5:
-                enforcou = True
-                print("ENFORCOU. Fim de jogo!")
-                print("A palavra secreta era: {}".format(palavra_secreta))
-                
-            tentativa =  tentativa + 1
+                pos = pos + 1    
+    
+            tentativa = tentativa + 1
             print("Resultado: {}".format(letra_posicao))
+
+        elif "_" in letra_posicao and tentativa > tamanho_palavra:
+            enforcou = True
+            print("ENFORCOU. Fim de jogo!")
+            print("A palavra secreta era: {}".format(palavra_secreta))      
 
         #Se acertou, mostra a palavra e finaliza o jogo    
         else:
@@ -60,4 +53,4 @@ def jogar():
 if(__name__ == "__main__"):
     jogar()
 
-    
+    #TO DO: identificar se o jogador já não tem mais chances e, caso afirmativo, encerrar o jogo
